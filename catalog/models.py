ba472b0049b4dfa7ea474e9de812503d829c8bf5
Waseem <mdvaseem014@gmail.com>
2020-09-14 15:01:21 +0530

    MyModel and my_field created..

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class MyModelName(models.Model):
+        my_field_name = models.CharField(max_length=20, help_text='Enter Field documentation')
+
+        class Meta:
+            ordering = ['-my_field_name']
+
+        def get_absolute_url(self):
+            return reverse('model-detail-view', args = [str(self.id)])
+
+        def __str__(self):
+            return self.my_field_name
